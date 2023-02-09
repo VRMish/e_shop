9324f6cb5ab74abb843a3914d29200b169a71216
VRMish <87526483+VRMish@users.noreply.github.com>
2023-02-09 16:08:49 +0530

    Update settings.py

--- E_shop/settings.py
+++ E_shop/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SEC_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['e-shop-mf0f.onrender.com']
 
 
 # Application definition
