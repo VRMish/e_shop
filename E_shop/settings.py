3f457f1216fa304b838531696f2c307d1185b081
VRMish <87526483+VRMish@users.noreply.github.com>
2023-02-09 15:40:05 +0530

    Update settings.py

--- E_shop/settings.py
+++ E_shop/settings.py
@@ -38,7 +38,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    "whitenoise.runserver_nostatic",
+    'whitenoise.runserver_nostatic',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
